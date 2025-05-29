83a79899dc8604d5cb78f69b1627395ac56e8f32
Jay <lj7812@gmail.com>
2025-05-29 23:09:41 +0900

    기능 구현 (상품 수정) (2), (상품 등록)

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,9 +6,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -46,5 +44,17 @@ public class BasicItemController {
         Item item = repository.findById(itemId);
         model.addAttribute("item", item);
         return "basic/editForm";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@ModelAttribute Item item, Model model) {
+        Item savedItem = repository.save(item);
+        model.addAttribute("item", savedItem);
+        return "basic/item";
     }
 }
