36a9d5c7bfe6f4fc62c7a0c074c5cfa73d1eb0fb
Jay <lj7812@gmail.com>
2025-06-17 22:06:38 +0900

    Chapter 2. Member 기능 구현 (1) - Model, Repository, Controller

---
+++ src/main/java/hello/itemservice/domain/member/MemberRepository.java
@@ -0,0 +1,48 @@
+package hello.itemservice.domain.member;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Slf4j
+@Repository
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long seq = 0L;
+
+    public Member save(Member member) {
+        member.setId(++seq);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(long id) {
+        return store.get(id);
+    }
+
+    public Optional<Member> findByLoginId(String loginId) {
+
+        // as-is
+        /*
+        List<Member> members = findAll();
+        for (Member member : members) {
+            if (member.getLoginId().equals(loginId)) {
+                return Optional.of(member);
+            }
+        }
+        return Optional.empty();
+        */
+
+        // to-be
+        return findAll()
+                .stream()
+                .filter(member -> member.getLoginId().equals(loginId))
+                .findFirst();
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<Member>(store.values());
+    }
+}
